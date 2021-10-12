bacdbcf6beff9882b1d14d8392f5073581d988bd
jhong.tao <jhong.tao@qq.com>
2021-10-12 08:30:39 +0800

    web

--- webofsecience/2.js
+++ webofsecience/2.js
@@ -1,7 +1,7 @@
 /*
- * @Author: your name
+ * @Author: jhong.tao
  * @Date: 2021-10-11 19:56:14
- * @LastEditTime: 2021-10-12 08:26:26
+ * @LastEditTime: 2021-10-12 08:30:22
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \webofsecience\1.js
@@ -28,45 +28,47 @@ var axios = document.createElement('script');
 axios.src = 'https://unpkg.com/axios/dist/axios.min.js';
 document.getElementsByTagName('head')[0].appendChild(axios);
 
-// md5 加密函数
-function getMd5(str){
-    var  def = $.Deferred();
-    url = "https://md5.justyy.workers.dev/?cached&s="+str;
-    $.get(url,function(data){
-        def.resolve(data);
-    })
-    return def.promise();
-}
-
-async function getTrans(query){
+// 翻译函数Promise版本
+function getTrans(query){
     var appid = '20210914000943517';    // 百度的api账号
     var key = 'zdhKVWmSlAPmXItiE4zh';      // 密钥
     var from = 'en';
     var to = 'zh';
     var salt = (new Date).getTime();      // 产生一个由时间生成的随机值，百度api要用
     var str1 = appid + query + salt +key; // 生成百度api md5加密要求字符串
-    const sign = await getMd5(str1);      // md5加密,当然如果md5.js在本地也可以通过ajax跨域的方式获取
+//     const sign = await getMd5(str1);      // md5加密,当然如果md5.js在本地也可以通过ajax跨域的方式获取
     return new Promise(function(resolve){
+        var url_md5 = "https://md5.justyy.workers.dev/?cached&s="+str1;
+        // md5加密
         $.ajax({
-        async:false,
-        url: 'https://fanyi-api.baidu.com/api/trans/vip/translate',
-        type: 'get',
-        dataType: 'jsonp',
-        data: {
-            q: query,
-            appid: appid,
-            salt: salt,
-            from: from,
-            to: to,
-            sign: sign
-        },
-        success: function(data) {
-            alert(data.length)
-            resolve(data.trans_result[0].dst);
-        }
-        });
+            async:false,
+            url:url_md5,
+            type:"get",
+            success:function(resp){
+                var sign = resp;
+                // 获取翻译结果
+                $.ajax({
+                    async:false,
+                    url: 'https://fanyi-api.baidu.com/api/trans/vip/translate',
+                    type: 'get',
+                    dataType: 'jsonp',
+                    data: {
+                        q: query,
+                        appid: appid,
+                        salt: salt,
+                        from: from,
+                        to: to,
+                        sign: sign
+                    },
+                    success: function(data) {
+                        resolve(data.trans_result[0].dst);
+                    }
+                    });
+            }
+        })
     });  
 };
+
 
 // 翻译每个标题
 async function requestSomething(){
@@ -109,68 +111,8 @@ async function requestSomething(){
 };
 
 
-function ajax(url, data, callback) {
-    var p = new Promise(function (resolve, reject) {
-        $.ajax({
-            url: url,
-            type: data == null ? 'GET' : 'POST',
-            dataType: "json",
-            data: data == null ? '' : JSON.stringify(data),
-            async: true,
-            contentType: "application/json",
-            success: function (resp) {
-                callback(resp);
-                resolve();
-            },
-            error: function (XMLHttpRequest, textStatus, errorThrown) {
-                if (XMLHttpRequest.status == "401") {
-                    window.parent.location = '/enterprise/enterprise_login.html';
-                    self.location = '/enterprise/enterprise_login.html';
-                } else {
-                    alert(XMLHttpRequest.responseText);
-                }
-                reject();
-            }
-        });
-    });
-    return p;
-}
 
-
-//url：地址
-//data：数据对象，在函数内部会转化成json串，如果没传，表示用GET方法，如果传了，表示用POST方法
-function ajax(url, data, callback) {
-    $.ajax({
-        url: url,
-        type: data == null ? 'GET' : 'POST',
-        dataType: "json",
-        data: data == null ? '' : JSON.stringify(data),
-        async: true,
-        contentType: "application/json",
-        success: function (resp) {
-            callback(resp);
-        },
-        error: function (XMLHttpRequest, textStatus, errorThrown) {
-            if (XMLHttpRequest.status == "401") {
-                window.parent.location = '/enterprise/enterprise_login.html';
-                self.location = '/enterprise/enterprise_login.html';
-            } else {
-                alert(XMLHttpRequest.responseText);
-            }
-        }
-    });
-}
-
-ajax(url,data,function(){
-    dosomething
-}).then(
-    ajax(url, data, function(){
-        do somth
-    }).then().then()
-)
-
-
-
+// 翻译函数
 function getTrans(query){
     var appid = '20210914000943517';    // 百度的api账号
     var key = 'zdhKVWmSlAPmXItiE4zh';      // 密钥
@@ -203,41 +145,3 @@ function getTrans(query){
     })
 };
 
-
-function getTrans(query){
-    var appid = '20210914000943517';    // 百度的api账号
-    var key = 'zdhKVWmSlAPmXItiE4zh';      // 密钥
-    var from = 'en';
-    var to = 'zh';
-    var salt = (new Date).getTime();      // 产生一个由时间生成的随机值，百度api要用
-    var str1 = appid + query + salt +key; // 生成百度api md5加密要求字符串
-//     const sign = await getMd5(str1);      // md5加密,当然如果md5.js在本地也可以通过ajax跨域的方式获取
-    return new Promise(function(resolve){
-        var url_md5 = "https://md5.justyy.workers.dev/?cached&s="+str1;
-        $.ajax({
-            async:false,
-            url:url_md5,
-            type:"get",
-            success:function(resp){
-                var sign = resp;
-                $.ajax({
-                    async:false,
-                    url: 'https://fanyi-api.baidu.com/api/trans/vip/translate',
-                    type: 'get',
-                    dataType: 'jsonp',
-                    data: {
-                        q: query,
-                        appid: appid,
-                        salt: salt,
-                        from: from,
-                        to: to,
-                        sign: sign
-                    },
-                    success: function(data) {
-                        resolve(data.trans_result[0].dst);
-                    }
-                    });
-            }
-        })
-    });  
-};
